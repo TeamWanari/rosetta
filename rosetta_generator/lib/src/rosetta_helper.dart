f774bf8c7820363e26d5754679041477702139f1
Agocs | MobileDev | Wanari <agocs@wanari.com>
2019-01-27 18:25:47 +0100

    Refactor generator logic
    Issue #14
    
    Add generator support for `Intercept` anotations.

--- rosetta_generator/lib/src/rosetta_helper.dart
+++ rosetta_generator/lib/src/rosetta_helper.dart
@@ -1,16 +1,17 @@
 part of 'rosetta_generator.dart';
 
-Class generateHelper(Element element, String path, List<String> keys) {
+Class generateHelper(String className, String path,
+    Map<String, List<String>> keyMap, List<MethodElement> interceptors) {
   return Class(
     (b) => b
       ..docs.addAll([
         "/// Loads and allows access to string resources provided by the JSON",
         "/// for the specified [Locale].",
         "///",
-        "/// Should be used as a mixin class for [${element.name}].",
+        "/// Should be used as a mixin class for [$className}].",
       ])
       ..abstract = true
-      ..name = "_\$${element.name}Helper"
+      ..name = "_\$${className}Helper"
       ..fields.add(
         Field((fb) => fb
           ..name = _translationsFieldName
@@ -18,7 +19,21 @@ Class generateHelper(Element element, String path, List<String> keys) {
       )
       ..methods.add(generateLoader(path))
       ..methods.add(generateTranslationMethod())
-      ..methods.addAll(generateKeyTranslationMethods(keys)),
+      ..methods.update((methods) {
+        if (interceptors.isEmpty) {
+          methods.addAll(generateSimpleGetterMethods(keyMap.keys.toList()));
+        } else {
+          methods.addAll(generateInterceptorMethods(interceptors));
+
+          sortKeysByInterceptors(keyMap, interceptors).forEach((method, keys) {
+            if (method != null) {
+              methods.addAll(generateInterceptedMethods(method, keys));
+            } else {
+              methods.addAll(generateSimpleGetterMethods(keys));
+            }
+          });
+        }
+      }),
   );
 }
 
@@ -75,17 +90,126 @@ Method generateTranslationMethod() {
   );
 }
 
-List<Method> generateKeyTranslationMethods(List<String> keys) {
+List<Method> generateInterceptorMethods(List<MethodElement> methods) => methods
+    .map((me) => Method((mb) => mb
+      ..name = me.name
+      ..docs.addAll(["/// Abstract Interceptor method."])
+      ..types.addAll(me.typeParameters.map((tp) => refer(tp.name)).toList())
+      ..requiredParameters.addAll(me.parameters
+          .map((pe) => Parameter((pb) => pb
+            ..name = pe.name
+            ..type = refer(pe.type.displayName)))
+          .toList())
+      ..returns = refer(me.returnType.name)))
+    .toList();
+
+List<Method> generateSimpleGetterMethods(List<String> keys) {
+  bool isFirst = true;
+
+  return keys
+      .map(
+        (key) => Method(
+              (mb) => mb
+                ..name = ReCase(key).camelCase
+                ..update((m) {
+                  if (isFirst) {
+                    isFirst = false;
+                    m.docs.addAll(["/// Simple getter methods"]);
+                  }
+                })
+                ..type = MethodType.getter
+                ..lambda = true
+                ..body = translate.call([
+                  refer(_keysClassName).property(ReCase(key).camelCase),
+                ]).code
+                ..returns = stringType,
+            ),
+      )
+      .toList();
+}
+
+List<Method> generateInterceptedMethods(
+    MethodElement interceptor, List<String> keys) {
+  if (interceptor.parameters.length == 1) {
+    return generateSimpleInterceptedMethods(refer(interceptor.name), keys);
+  } else if (interceptor.parameters.length > 1) {
+    return generateParametrizedInterceptedMethods(interceptor, keys);
+  }
+  return <Method>[];
+}
+
+List<Method> generateSimpleInterceptedMethods(
+    Reference interceptor, List<String> keys) {
+  bool isFirstMethod = true;
+
   return keys
       .map(
         (key) => Method(
               (mb) => mb
                 ..name = ReCase(key).camelCase
                 ..type = MethodType.getter
+                ..update((m) {
+                  if (isFirstMethod) {
+                    isFirstMethod = false;
+                    m.docs.addAll([
+                      "/// Simple getter methods for [${interceptor.code.toString()}] interceptor."
+                    ]);
+                  }
+                })
                 ..lambda = true
-                ..body = translate.call([
-                  refer(_keysClassName).property(ReCase(key).camelCase),
+                ..body = interceptor.call([
+                  translate.call([
+                    refer(_keysClassName).property(ReCase(key).camelCase),
+                  ]),
                 ]).code
+                ..returns = stringType,
+            ),
+      )
+      .toList();
+}
+
+List<Method> generateParametrizedInterceptedMethods(
+    MethodElement interceptor, List<String> keys) {
+  var interceptorMethod = refer(interceptor.name);
+  var methodParameters = interceptor.parameters
+      .skip(1)
+      .map((e) => Parameter((pb) => pb
+        ..name = e.name
+        ..type = refer(e.type.displayName)))
+      .toList();
+
+  var internalParameters =
+      interceptor.parameters.skip(1).map((e) => refer(e.name)).toList();
+
+  bool isFirstMethod = true;
+
+  return keys
+      .map(
+        (key) => Method(
+              (mb) => mb
+                ..name = ReCase(key).camelCase
+                ..update((m) {
+                  if (isFirstMethod) {
+                    isFirstMethod = false;
+                    m.docs.addAll([
+                      "/// Parametrized methods for [${interceptor.name}] interceptor."
+                    ]);
+                  }
+                })
+                ..requiredParameters.addAll(methodParameters)
+                ..lambda = true
+                ..types.addAll(interceptor.typeParameters
+                    .map((tp) => refer(tp.name))
+                    .toList())
+                ..body = interceptorMethod
+                    .call(
+                      List()
+                        ..add(translate.call([
+                          refer(_keysClassName).property(ReCase(key).camelCase),
+                        ]))
+                        ..addAll(internalParameters),
+                    )
+                    .code
                 ..returns = stringType,
             ),
       )
