ff02ac93c9a68df3fe647b82becf80120f206787
Agocs | MobileDev | Wanari <agocs@wanari.com>
2019-02-24 19:42:29 +0100

    Extend Stone annotation for package support
    Issue #23
    
    Optional `package` attribute added for `Stone` annotation.

--- rosetta/lib/src/stone.dart
+++ rosetta/lib/src/stone.dart
@@ -7,9 +7,20 @@ class Stone {
   /// Must not be [null].
   final String path;
 
+  /// Optional. Indicates which package bundles the referenced
+  /// JSON files.
+  ///
+  /// In multi-package Flutter projects, if you declare a [Stone]
+  /// annotated class, which uses JSON assets from a package other
+  /// then the root package, you must declare the resource's host
+  /// package or the Localization library will try to load your
+  /// JSON assets from the root bundle.
+  final String package;
+
   /// Create an annotation that will generate the Helper and
   /// Delegate classes for the translations located at [path].
   const Stone({
     this.path,
+    this.package,
   }) : assert(path != null);
 }
